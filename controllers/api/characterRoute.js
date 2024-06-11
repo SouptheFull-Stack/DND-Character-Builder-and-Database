917d422c01cd3c86d422e0c0fb423efd58f80c5f
illakaya <mimijane.nguyen@gmail.com>
2024-06-11 20:06:47 +1000

    edited char model and added index

---
+++ controllers/api/characterRoute.js
@@ -0,0 +1,15 @@
+// initialise express
+const router = require('express').Router();
+// retrieve models
+const { Character } = require('../../models');
+
+// create a new character
+router.post('/', async (req, res) => {
+  try {
+    const dbCharacterData = await Character.create();
+    res.status(200).json(dbCharacterData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
