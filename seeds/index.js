89343cbf241889582d1ed09518fdc10b3017ff16
Mitra USYD <mitraahmadi129@gmail.com>
2024-06-13 19:47:54 +1000

    adding in the index.js seeding function for the seeds folder hopefully ready for testing with insomnia

---
+++ seeds/index.js
@@ -0,0 +1,41 @@
+const sequelize = require("../config/connection");
+
+// const Character = require("../models/Character");
+// const charData = require("./characterData.json");
+
+const User = require("../models/User");
+const userData = require("./userData.json");
+
+const Class = require("../models/Class");
+const classData = require("./classData.json");
+
+const Race = require("../models/Race");
+const raceData = require("./raceData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  //   await Character.bulkCreate(charData, {
+  //     individualHooks: true,
+  //     returning: true,
+  //   });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Class.bulkCreate(classData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Race.bulkCreate(raceData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
