b5d161bc5ebb7df03647e0d680ff04a94b18e38d
ZACGIL <zac.gilfoyle@gmail.com>
2024-06-13 22:18:23 +1000

    home routes added and console link for server.js

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,8 +1,54 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
-// Setting up rendering template for the homepage with sequelize
-router.get("/", async (req, res) => {
-  res.render("homepage");
+router.get('/', withAuth, async (req, res) => {
+  try {
+    res.render('homepage');
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', withAuth, async (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+    
+    res.render('login');
+});
+
+router.get('/profile', (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+  
+  res.render('profile');
+});
+
+router.get('/info', (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
+  res.render('info');
+});
+
+router.get('/create', (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
+  res.render('create');
+});
+
+router.get('/logout', (req, res) => {
+
+  res.render('homepage');
 });
 
 module.exports = router;
